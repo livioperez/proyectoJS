a3dc220be47bded8cfd38ed75da97ebe6ad20683
livioperez <daniellperez@hotmail.com>
2022-04-19 18:55:58 -0300

    proyecto

---
+++ archivo.js
@@ -0,0 +1,35 @@
+
+
+ let comida;
+
+document.querySelector("#btnPrinsipal1").addEventListener("click", () => { comida = 
+document.querySelector("#eleccion1").value;
+document.querySelector("#aprueba1").value = comida;}); 
+
+
+
+
+
+
+let bebida;
+
+document.querySelector("#btnPrinsipal2").addEventListener("click", () => { bebida = 
+document.querySelector("#eleccion2").value;
+document.querySelector("#aprueba2").value = bebida;}); 
+
+
+let postre;
+
+document.querySelector("#btnPrinsipal3").addEventListener("click", () => { postre = 
+document.querySelector("#eleccion3").value;
+document.querySelector("#aprueba3").value = postre;}); 
+
+
+let abonar;
+
+document.querySelector("#btnPrinsipal4").addEventListener("click", () => { abonar = 
+document.querySelector("#eleccion4").value;
+document.querySelector("#aprueba4").value = abonar;}); 
+
+
+
